cffa0c6d0c3af7a0ac65dab12b9f131b8cd62eee
StarssssST <starssxproject@gmail.com>
2025-02-22 11:49:30 +0700

    jadi gini

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the API' });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
